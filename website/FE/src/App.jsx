f57109aa1840f9284a80a82cc60daa594fe934df
alfharisqi <risqiwicaksonoalfha@gmail.com>
2026-07-27 11:00:42 +0700

    menambahkan page workshop

--- website/FE/src/App.jsx
+++ website/FE/src/App.jsx
@@ -3,6 +3,7 @@ import { Home } from './pages/Home.jsx';
 import { Ide } from './pages/Ide.jsx';
 import { Access } from './pages/Access.jsx';
 import { Program } from './pages/Program.jsx';
+import { Workshop } from './pages/Workshop.jsx';
 import { Tutorial } from './pages/Tutorial.jsx';
 import { Project } from './pages/Project.jsx';
 import { Partner } from './pages/Partner.jsx';
@@ -14,6 +15,7 @@ const routes = {
   '/ide': Ide,
   '/akses': Access,
   '/program': Program,
+  '/workshop': Workshop,
   '/tutorial': Tutorial,
   '/project': Project,
   '/partner': Partner,
