7754f7283fc31e080fb975e4303038423a2e3a3a
gepenkdimas639@gmail.com <gepenkdimas639@gmail.com>
2026-08-31 14:01:09 +0700

    update terbaru artikel dan materi

--- website/BE/config/database.php
+++ website/BE/config/database.php
@@ -6,8 +6,12 @@ use Arduflow\Api\Support\Env;
 
 return [
     'sqlite' => [
-        'path' => Env::get('SQLITE_DATABASE_PATH', __DIR__ . '/../storage/database/arduflow.sqlite'),
-        'busy_timeout_ms' => Env::int('SQLITE_BUSY_TIMEOUT_MS', 5000),
+        'path' => dirname(__DIR__)
+            . DIRECTORY_SEPARATOR . 'storage'
+            . DIRECTORY_SEPARATOR . 'database'
+            . DIRECTORY_SEPARATOR . 'arduflow.sqlite',
+
+        'busy_timeout_ms' => 15000,
     ],
 
     'mysql' => [
@@ -16,6 +20,9 @@ return [
         'database' => Env::get('DB_DATABASE', 'db_arduflow'),
         'username' => Env::get('DB_USERNAME', 'root'),
         'password' => Env::get('DB_PASSWORD', ''),
-        'connect_timeout_seconds' => Env::int('DB_CONNECT_TIMEOUT_SECONDS', 3),
+        'connect_timeout_seconds' => Env::int(
+            'DB_CONNECT_TIMEOUT_SECONDS',
+            3
+        ),
     ],
 ];
